3b977062202b82ad2e85d85a1d0a77d23c1edd6c
Dawid Kondraszuk <dkondraszuk@luxoft.com>
2019-07-19 11:34:33 +0200

    Ready list with input text

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Install requirements:\
 pip install -r /path/to/requirements.txt
 
 Required:\
-Google Chrome Web Browser
+Mozilla Firefox Web Browser
--- typing_bot.py
+++ typing_bot.py
@@ -1,41 +1,25 @@
-# Standard library imports...
 import time
 
-# Third-party imports...
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+# from selenium.webdriver.firefox.options import Options
 
-# Local imports...
-from urls import LOGGED_IN_URL, CHROME_EXE_PATH, FIREFOX_EXE_PATH
+from urls import LOGGED_IN_URL, FIREFOX_EXE_PATH
 from pages.account_page import AccountPage
 from pages.practice_page import PracticePage
 
 # options = Options()
 # options.headless = True
 
-# Chrome
-# driver = webdriver.Chrome(executable_path=CHROME_EXE_PATH)
-
-# Firefox
 driver = webdriver.Firefox(executable_path=FIREFOX_EXE_PATH)
+
 driver.set_window_size(1440, 900)
-# driver.implicitly_wait(10)
-# driver.set_page_load_timeout(15)
-# driver.get(LOGGED_IN_URL)
-
 driver.get(LOGGED_IN_URL)
-
 print('Logged In')
-# driver.find_elements_by_class_name('TextInput-item').text
+
 # go to practice page
 account = AccountPage(driver)
 account.click_practice()
 print('Went to Practice')
-
 
 practice = PracticePage(driver)
 
@@ -43,10 +27,10 @@ practice = PracticePage(driver)
 practice.close_popup_window()
 
 # get TextInput items
-text_input_items = practice.find_text_input_items()
-for item in text_input_items:
-    print(item.text)
+input_items = practice.find_text_input_items()
+input_items_text = [' ' if item.text == '␣' else item.text for item in input_items]
+input_text = ''.join(input_items_text)
+print(input_text)
 
-time.sleep(120)
-
+time.sleep(5)  # fixme: only for testing
 driver.quit()
