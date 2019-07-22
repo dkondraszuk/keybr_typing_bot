10c975505b24c55694e312a21954ac3794ced065
dkondraszuk <prawdydawca@gmail.com>
2019-07-23 00:40:54 +0200

    Implemented keypress_emulator, working bot - first issue

---
+++ keypress_emulator.py
@@ -0,0 +1,22 @@
+import time
+import random
+
+from pynput.keyboard import Controller
+
+
+class Keyboard:
+
+    def __init__(self):
+        self._keyboard = Controller()
+
+    @staticmethod
+    def _generate_typing_delay():
+        delay = random.uniform(0.06, 0.12)
+        return round(delay, 3)
+
+    def type_sentence(self, sentence):
+        for char in sentence:
+            self._keyboard.press(char)
+            self._keyboard.release(char)
+            delay = self._generate_typing_delay()
+            time.sleep(delay)
