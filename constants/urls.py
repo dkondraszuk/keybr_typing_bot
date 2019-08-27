9ef34fd3f9303c693cba7d6d4933fc82da82e1d0
Dawid Kondraszuk <dkondraszuk@luxoft.com>
2019-08-27 12:27:44 +0200

    Little cleanup

--- constants/urls.py
+++ constants/urls.py
@@ -7,10 +7,6 @@ AUTH_KEY = os.environ.get('KEYBR_KEY')
 
 LOGGED_IN_URL = urllib.parse.urljoin(BASE_URL, AUTH_KEY)
 
-# Chrome exe path:
-CHROME_DRIVER = 'drivers/chromedriver'
-CHROME_EXE_PATH = os.path.abspath(CHROME_DRIVER)
-
 # Firefox exe path:
 FIREFOX_DRIVER = 'drivers/geckodriver'
 FIREFOX_EXE_PATH = os.path.abspath(FIREFOX_DRIVER)
